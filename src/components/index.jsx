55a8fc1d12708ecb662d0a42cd227d4507a3ede0
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-02-02 13:17:00 +0530

    Create SCss Files

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./style.scss";
+import cloud from "../../src/images/cloud.png";
+import cloudSoft from "../../src/images/cloud-soft.png";
+
 
 const Intro = () => {
   return (
-    <div>
-      <p>Intro</p>
+    <div className="Intro_section">
+        <div className="vector_bg" id='parallex'>
+
+        </div>
+
+        <img  src={cloud} className = "cloud-soft"  alt=''/>
+        <img  src={cloudSoft} className = "cloud-soft"  alt=''/>
+    <div className="contennt">
+      
+    </div>
     </div>
   )
 }
