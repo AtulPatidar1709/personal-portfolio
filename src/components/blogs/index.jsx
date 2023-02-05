89b102697835be20b86b97a2ec30d244f2e24fa9
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-02-05 11:52:41 +0530

    Changes in blog card

--- src/components/blogs/index.jsx
+++ src/components/blogs/index.jsx
@@ -10,9 +10,9 @@ const Blogs = () => {
   return (
     <Section id="blogs" title="Blogs & Articles" background="dark">
     <div className="blogs-content-wrapper">
-    <BlogCard  user="Atul Patidar" date="01 Jan 2023" image={Blog1} title="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "  />
-    <BlogCard  user="Atul Patidar" date="07 Jan 2023" image={Blog2} title="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "  />
-    <BlogCard  user="Atul Patidar" date="15 Jan 2023" image={Blog3} title="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "  />
+    <BlogCard  user="Atul Patidar" date="01 Jan 2023" image={Blog1} title="Neque porro quisquam est qui dolorem " description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "  />
+    <BlogCard  user="Atul Patidar" date="07 Jan 2023" image={Blog2} title="Neque porro quisquam est qui dolorem " description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "  />
+    <BlogCard  user="Atul Patidar" date="15 Jan 2023" image={Blog3} title="Neque porro quisquam est qui dolorem " description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "  />
     </div>
     </Section>
   )
